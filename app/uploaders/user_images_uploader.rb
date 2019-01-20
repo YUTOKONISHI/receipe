bc23f97ee91be0bfab282b2659049235e55c84ec
yuto <romance.saga.kony@gmail.com>
2019-01-20 14:51:27 +0900

    Test1-13

--- app/uploaders/user_images_uploader.rb
+++ app/uploaders/user_images_uploader.rb
@@ -13,6 +13,7 @@ class UserImagesUploader < CarrierWave::Uploader::Base
    storage :fog
   end
 
+  storage :file
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,22 +1,21 @@
-require 'carrierwave/storage/abstract'
-require 'carrierwave/storage/file'
-require 'carrierwave/storage/fog'
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: 'AKIAJY4ZDWJOAQ3LNC4A',
-    aws_secret_access_key: '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',
-    region: 'ap-northeast-1'
-  }
+if Rails.env.development?
+  
+elsif Rails.env.test?
+ 
+else
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: 'AKIAJY4ZDWJOAQ3LNC4A',
+      aws_secret_access_key: '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',
+      region: 'ap-northeast-1'
+    }
 
-  config.fog_directory  = 'allergen-free-dish'
-  config.asset_host = "https://s3.ap-northeast-1.amazonaws.com/allergen-free-dish"
-  if Rails.env.development?
-   config.cache_storage = :file
-  elsif Rails.env.test?
-   config.cache_storage = :file
-  else
-   config.cache_storage = :fog
-  end
+    config.fog_directory  = 'allergen-free-dish'
+    config.asset_host = "https://s3.ap-northeast-1.amazonaws.com/allergen-free-dish"
+
   #config.fog_provider = 'fog/aws'
+  end  
+ #config.cache_storage = :fog
 end
+
